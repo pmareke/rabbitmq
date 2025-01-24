e565c64d9e1ec3e7581ad45282c4fe7c2941920a
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-01-24 20:35:08 +0100

    feat: fix printer function

--- consumer/main.py
+++ consumer/main.py
@@ -1,4 +1,17 @@
+from pika import BasicProperties
+from pika.spec import Basic, Channel
+
 from src.consumer import Consumer
 
-consumer = Consumer(lambda message: print(message))
+
+def printer(
+    channel: Channel,
+    method: Basic.Deliver,
+    props: BasicProperties,
+    body: str,
+) -> None:
+    print(body)
+
+
+consumer = Consumer(printer)
 consumer.start()
